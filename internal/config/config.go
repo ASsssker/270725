f304cdc5e3c62d53a50c158b30eb20a260f70a84
ASsssker <ask.mol2@mail.ru>
2025-07-27 23:03:33 +0300

    add: task config and filter config

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"github.com/go-playground/validator/v10"
 	"github.com/ilyakaznacheev/cleanenv"
 	"time"
 )
@@ -16,28 +17,38 @@ const (
 )
 
 type Config struct {
-	LogLevel string `env:"LOG_LEVEL" envDefault:"info" validate:"oneof=debug info warn error"`
+	LogLevel LogLevel `env:"LOG_LEVEL" env-default:"info" validate:"oneof=debug info warn error"`
 	ServerConfig
 	TaskConfig
+	Filter
 }
 
 type ServerConfig struct {
-	Host         string        `env:"HOST" envDefault:"127.0.0.1"`
-	Port         string        `env:"PORT" envDefault:"8080"`
-	ReadTimeout  time.Duration `env:"READ_TIMEOUT" envDefault:"5s"`
-	WriteTimeout time.Duration `env:"WRITE_TIMEOUT" envDefault:"5s"`
-	IdleTimeout  time.Duration `env:"IDLE_TIMEOUT" envDefault:"10"`
+	Host         string        `env:"HOST" env-default:"127.0.0.1"`
+	Port         string        `env:"PORT" env-default:"8080"`
+	ReadTimeout  time.Duration `env:"READ_TIMEOUT" env-default:"5s"`
+	WriteTimeout time.Duration `env:"WRITE_TIMEOUT" env-default:"5s"`
+	IdleTimeout  time.Duration `env:"IDLE_TIMEOUT" env-default:"10s"`
 }
 
 type TaskConfig struct {
-	TasksBufferSize uint `env:"TASKS_BUFFER_SIZE" envDefault:"3"`
-	LinksInTask     uint `env:"LINKS_IN_TASK" envDefault:"3"`
+	TasksBufferSize uint `env:"TASKS_BUFFER_SIZE" env-default:"3"`
+	LinksInTask     uint `env:"LINKS_IN_TASK" env-default:"3"`
+}
+
+type Filter struct {
+	AllowedExtensions []string `env:"ALLOWED_EXTENSIONS" env-default:"jpg,png,pdf"`
 }
 
 func MustLoad() Config {
 	var cfg Config
 	if err := cleanenv.ReadEnv(&cfg); err != nil {
 		panic(fmt.Errorf("failed to load config: %w", err))
+	}
+
+	validate := validator.New()
+	if err := validate.Struct(cfg); err != nil {
+		panic(fmt.Errorf("failed to validate config: %w", err))
 	}
 
 	return cfg
