72ab6fe7506a0811b61b43a35f04702ac51fe433
ASsssker <ask.mol2@mail.ru>
2025-07-27 23:04:33 +0300

    add: zipper

---
+++ internal/services/zipper_service.go
@@ -0,0 +1,48 @@
+package services
+
+import (
+	"archive/zip"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+type Zipper struct {
+	archivePath string
+}
+
+func NewZipper(archivePath string) (*Zipper, error) {
+	if err := os.MkdirAll(archivePath, os.ModePerm); err != nil {
+		return nil, fmt.Errorf("failed to create zipper directory: %w", err)
+	}
+
+	return &Zipper{
+		archivePath: archivePath,
+	}, nil
+}
+
+func (z *Zipper) ToArchive(archiveName string, files map[string][]byte) error {
+	archiveName = filepath.Join(archiveName, filepath.Base(z.archivePath))
+
+	archive, err := os.Create(archiveName)
+	if err != nil {
+		return fmt.Errorf("failed to create zipper file: %w", err)
+	}
+	defer archive.Close()
+
+	zipWriter := zip.NewWriter(archive)
+	defer zipWriter.Close()
+
+	for filename, data := range files {
+		w, err := zipWriter.Create(filename)
+		if err != nil {
+			return fmt.Errorf("failed to create zipWriter %w", err)
+		}
+
+		if _, err := w.Write(data); err != nil {
+			return fmt.Errorf("failed to write zipWriter %w", err)
+		}
+	}
+
+	return nil
+}
